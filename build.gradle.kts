a2fbe6a204520c523d518ef539bd559783912903
Yooshyasha <mejzikrejzi66@gmail.com>
2025-08-26 22:41:14 +0500

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation("com.google.api-client:google-api-client:2.2.0")
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
     implementation("com.google.http-client:google-http-client-jackson2:1.43.3")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.9.0")
 }
 
 kotlin {
