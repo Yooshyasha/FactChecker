cbfd74e3bde4cb687531a223635a4213ed1c4bde
Yooshyasha <mejzikrejzi66@gmail.com>
2025-08-26 17:24:04 +0500

    Init

---
+++ src/main/kotlin/com/yooshyasha/factcheckerpet/FactCheckerPetApplication.kt
@@ -0,0 +1,11 @@
+package com.yooshyasha.factcheckerpet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FactCheckerPetApplication
+
+fun main(args: Array<String>) {
+    runApplication<FactCheckerPetApplication>(*args)
+}
---
+++ src/test/kotlin/com/yooshyasha/factcheckerpet/FactCheckerPetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yooshyasha.factcheckerpet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FactCheckerPetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
