57d6cc36844ba3edbd8fb7f8e0d282c0089d5d9f
Yooshyasha <yooshyasha@proton.me>
2026-07-29 01:29:52 +0500

    fix

--- src/main/kotlin/com/yooshyasha/factcheckerpet/config/AgentConfig.kt
+++ src/main/kotlin/com/yooshyasha/factcheckerpet/config/AgentConfig.kt
@@ -1,8 +1,10 @@
 package com.yooshyasha.factcheckerpet.config
 
 import ai.koog.agents.mcp.McpToolRegistryProvider
-import ai.koog.prompt.executor.clients.anthropic.AnthropicModels
 import ai.koog.prompt.executor.llms.all.simpleAnthropicExecutor
+import ai.koog.prompt.llm.LLMCapability
+import ai.koog.prompt.llm.LLMProvider
+import ai.koog.prompt.llm.LLModel
 import io.modelcontextprotocol.kotlin.sdk.client.SseClientTransport
 import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Value
@@ -15,7 +17,18 @@ class AgentConfig(
 ) {
     // я бы сделал нормальный резолвер, но нет я тороплюсь
     @Bean
-    fun model() = AnthropicModels.Sonnet_4
+    fun model() = LLModel(
+        provider = LLMProvider.Anthropic,
+        id = "claude-3-5-haiku",
+        capabilities = listOf(
+            LLMCapability.Temperature,
+            LLMCapability.Tools,
+            LLMCapability.ToolChoice,
+            LLMCapability.Vision.Image,
+            LLMCapability.Schema.JSON.Full,
+            LLMCapability.Completion
+        )
+    )
 
     // todo(обновить koog на 1.0 для мультипровайдерности)
     @Bean
